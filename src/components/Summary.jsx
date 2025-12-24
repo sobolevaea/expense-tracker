1d570e82359ca8a53554b7c89a8055b7587203e7
sobolevaea <liziaka@mail.ru>
2025-12-24 19:29:50 +0300

    feat: add Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,32 @@
+import { CATEGORIES, getCategories } from "../constants/categories"
+
+import { ListGroup } from "react-bootstrap"
+
 const Summary = () => {
+  const categories = getCategories()
 
   return (
-    <></>
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12">
+          <div className="card">
+            <h5 className="card-header">Итого</h5>
+            <div className="card-body">
+              <div className="d-flex justify-content-center flex-column">
+                <h6 className='mb-0'>Всего потрачено: {1} ₽</h6>
+                <hr></hr>
+                <p className="mt-0 my-2">По категориям:</p>
+                <ListGroup className="my-2">
+                  {categories.map(c => (
+                    <ListGroup.Item key={c.id}><b>{c.name}:</b> {3000} ₽</ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
